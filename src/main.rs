2cf00a7f434d2cb9b0c22dedf987ac8dfa4327e8
sdp8554 <ihurtmyknee11@gmail.com>
2025-05-18 16:26:10 -0400

    made preperations to make a simple ground layer with the player spawned on it

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_systems(Startup, setup_test_level)
         .add_systems(Startup, setup_graphics)
-        .add_systems(Startup, setup_physics)
-        .add_systems(Update, print_ball_altitude)
         .run();
 }
+
+fn setup_test_level(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+){
+    commands.spawn(PointLight{intensity: 1000.0,..Default::default()});
+    let mesh_handle = meshes.add(Cuboid::new(1.0,1.0,1.0));
+    let material_handle = materials.add(StandardMaterial{
+        base_color: Color::LinearRgba(LinearRgba { red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0 }),..Default::default()
+    });
+    //spawn in ground layer; 50x50 meter
+    for i in 0..50{
+        for j in 0..50{
+            //TODO apply basic gpraphic to these colliders with PBR
+            //Have each row or coulmn have a unique color to better understand map positioning
+            //I want to see the coordinate plane
+            //spawn in colliders 
+            commands
+            .spawn(Collider::cuboid(0.5, 0.5, 0.5))
+            .insert(Transform::from_xyz(i as f32, j as f32, 0.0));
+            //spawn visual block for each
+            
+        }
+    }
+}
+
 
 fn setup_graphics(mut commands: Commands) {
     // Add a camera so we can see the debug-render.
